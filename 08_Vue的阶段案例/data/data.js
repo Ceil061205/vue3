0e2cd8a1ea51779ba92604558e7b4d019e6f25b6
ceil001 <2568333411@qq.com>
2026-01-10 18:19:17 +0800

    vue3 2(下)

---
+++ 08_Vue的阶段案例/data/data.js
@@ -0,0 +1,10 @@
+const books = [
+  { id: 1, name: "Book 1", date: "2023-01-01", price: 10.99, count: 10 },
+  { id: 2, name: "Book 2", date: "2023-01-02", price: 15.99, count: 5 },
+  { id: 3, name: "Book 3", date: "2023-01-03", price: 20.99, count: 2 },
+  { id: 4, name: "Book 4", date: "2023-01-04", price: 25.99, count: 8 },
+  { id: 5, name: "Book 5", date: "2023-01-05", price: 30.99, count: 1 },
+  { id: 6, name: "Book 6", date: "2023-01-06", price: 35.99, count: 3 },
+  { id: 7, name: "Book 7", date: "2023-01-07", price: 40.99, count: 6 },
+  { id: 8, name: "Book 8", date: "2023-01-08", price: 45.99, count: 4 }
+]
